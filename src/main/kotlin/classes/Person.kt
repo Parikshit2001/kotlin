57129f94b8d8ff10ab551dacd13860985ea85425
Parikshit Gupta <parikshit.gupta@personifyhealth.com>
2025-05-16 16:39:43 +0530

    primary constructors

--- src/main/kotlin/classes/Person.kt
+++ src/main/kotlin/classes/Person.kt
@@ -1,6 +1,6 @@
 package org.example.classes
 
-class Person {
+class Person(val name: String = "", val age: Int = 0) {
 
     fun action() {
         println("Person Walks")
@@ -8,6 +8,13 @@ class Person {
 }
 
 fun main() {
-    val person = Person()
+    val person = Person("Alex", 25)
     person.action()
+    println("Name: ${person.name}, Age: ${person.age}")
+
+    val person1 = Person()
+    println("Person1")
+    println("Name: ${person1.name}, Age: ${person1.age}")
+
+
 }
