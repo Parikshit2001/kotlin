8ae071b325a6ae63edc9aa08ec63f9c1440609e7
Parikshit Gupta <parikshit.gupta@personifyhealth.com>
2025-05-26 03:22:16 +0530

    init block

--- src/main/kotlin/classes/Person.kt
+++ src/main/kotlin/classes/Person.kt
@@ -3,6 +3,12 @@ package org.example.classes
 class Person(val name: String = "", val age: Int = 0) {
 
     var email: String = ""
+    var nameLength: Int = 0
+
+    init {
+        println("Initializing Person")
+        nameLength = name.length
+    }
 
     constructor(_email: String, _name: String = "", _age: Int = 0) : this(_name, _age) {
         email = _email
@@ -14,14 +20,14 @@ class Person(val name: String = "", val age: Int = 0) {
 }
 
 fun main() {
-    val person = Person("Alex", 25)
-    person.action()
-    println("Name: ${person.name}, Age: ${person.age}")
-
-    val person1 = Person()
-    println("Person1")
-    println("Name: ${person1.name}, Age: ${person1.age}")
+//    val person = Person("Alex", 25)
+//    person.action()
+//    println("Name: ${person.name}, Age: ${person.age}")
+//
+//    val person1 = Person()
+//    println("Person1")
+//    println("Name: ${person1.name}, Age: ${person1.age}")
 
     val person2 = Person(_email = "abc@gmail.com", _name = "abc", _age = 25)
-    println("Name: ${person2.name}, Age: ${person2.age}, Email: ${person2.email}")
+    println("Name: ${person2.name}, Age: ${person2.age}, Email: ${person2.email}, nameLength: ${person2.nameLength}")
 }
