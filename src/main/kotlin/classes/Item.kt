90f6ba33e20c918eab269282a3cf2215b4ba29a2
Parikshit Gupta <parikshit.gupta@personifyhealth.com>
2025-05-26 16:43:15 +0530

    Custom getter and setter

--- src/main/kotlin/classes/Item.kt
+++ src/main/kotlin/classes/Item.kt
@@ -2,6 +2,19 @@ package org.example.classes
 
 class Item() {
     var name: String = ""
+    var price: Double = 0.0
+        get() {
+            println("In getter")
+            return field
+        }
+        set(value) {
+            println("In setter")
+            if (value >= 0.0) {
+                field = value
+            } else {
+                throw IllegalArgumentException("value can't be more than 0.0")
+            }
+        }
 
     constructor(_name: String) : this() {
         name = _name
@@ -13,4 +26,6 @@ fun main() {
     val item = Item("IPhone")
     println(item.name)
     println(item)
+    item.price = 10.0
+    println(item.price)
 }
