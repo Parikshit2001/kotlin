38c24828f337f60766e95141b9dbaebeeb17bf7c
Parikshit Gupta <parikshit.gupta@personifyhealth.com>
2025-05-26 16:51:44 +0530

    Overriding functions and variables

--- src/main/kotlin/classes/Inheritance.kt
+++ src/main/kotlin/classes/Inheritance.kt
@@ -1,21 +1,30 @@
 package org.example.classes
 
 open class User(val name: String) {
-
-    fun login() {
+    open var isLoggedIn: Boolean = false
+    open fun login() {
         println("Insdie User Login")
     }
 }
 
-class Student(name: String) : User(name) {}
+class Student(name: String) : User(name) {
+    override var isLoggedIn: Boolean = true
+    override fun login() {
+        println("Inside Student login")
+    }
+
+}
+
 class Instructor(name: String) : User(name) {}
 
 fun main() {
     val student = Student("Alex")
     println(student.name)
     student.login()
+    println(student.isLoggedIn)
 
     val instructor = Instructor("Dilip")
     println(instructor.name)
     instructor.login()
+    println(instructor.isLoggedIn)
 }
