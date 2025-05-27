88c820586cc1c6945963615eaa28fcb6213637de
Parikshit Gupta <parikshit.gupta@personifyhealth.com>
2025-05-27 13:41:16 +0530

    interfaces

---
+++ src/main/kotlin/interfaces/Interfaces.kt
@@ -0,0 +1,52 @@
+package org.example.interfaces
+
+import org.example.classes.Course
+
+interface CourseRepository {
+    fun getById(id: Int): Course
+
+    fun save(course: Course): Int {
+        println("Course: $course")
+        return course.id
+    }
+}
+
+interface Repository {
+    fun getAll(): Any
+}
+
+class SqlCourseRepository : CourseRepository, Repository {
+    override fun getById(id: Int): Course {
+        return Course(id, "Reactive Programming", "Parik")
+    }
+
+    override fun getAll(): Any {
+        return 1
+    }
+}
+
+class NoSqlCourseRepository : CourseRepository {
+    override fun getById(id: Int): Course {
+        return Course(id, "Reactive Programming", "Parik")
+    }
+
+    override fun save(course: Course): Int {
+        println("Course in noSqlCourseRepository: $course")
+        return course.id
+    }
+}
+
+
+fun main() {
+    val sqlCourseRepository = SqlCourseRepository()
+    val course = sqlCourseRepository.getById(1)
+    println("Course is $course")
+    val courseID = sqlCourseRepository.save(Course(5, "Reactive Programming", "Parik"))
+    println("Saved courseId is $courseID")
+
+    val noSqlCourseRepository = NoSqlCourseRepository()
+    val course1 = noSqlCourseRepository.getById(1)
+    println("Course1 is $course1")
+    val course2 = noSqlCourseRepository.save(Course(6, "Reactive Programming", "Parik"))
+    println("Saved courseId in noSQLCourseRepository is $course2")
+}
