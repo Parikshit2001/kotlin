f5c89aafb51165de30686993bbf40294788a3c91
Parikshit Gupta <parikshit.gupta@personifyhealth.com>
2025-05-27 15:32:52 +0530

    enum class

--- src/main/kotlin/classes/Course.kt
+++ src/main/kotlin/classes/Course.kt
@@ -4,11 +4,19 @@ data class Course(
     val id: Int,
     val name: String,
     val author: String,
+    val courseCategory: CourseCategory = CourseCategory.DEVELOPMENT
 )
+
+enum class CourseCategory {
+    DEVELOPMENT,
+    BUSINESS,
+    DESIGN,
+    MARKETING
+}
 
 fun main() {
     val course = Course(1, "Reactive Programming", "Parik")
-//    println(course)
+    println(course)
 
     val course1 = Course(1, "Reactive Programming", "Parik")
 
@@ -16,4 +24,7 @@ fun main() {
 
     val course3 = course1.copy(id = 3, name = "Dilip")
     println(course3)
+
+    val marketingCourse = Course(1, "Reactive Programming", "Parik", CourseCategory.MARKETING)
+    println(marketingCourse)
 }
